c72cec13f18b72d978b655f8ec8aa58bf433884e
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2020-04-16 23:51:06 +0600

    mon

--- MononSoftLtd/app/src/main/java/com/mononsoft/mononsoftltd/LoginActivity.java
+++ MononSoftLtd/app/src/main/java/com/mononsoft/mononsoftltd/LoginActivity.java
@@ -4,16 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private FirebaseUser currentUser;
+    private Button LoginButton, PhoneLoginButton;
+    private EditText UserEmail, UserPassword;
+    private TextView NeedNewAccountLink, forgetPasswordLink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        InitializeFields();
+
+        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  SendUserToRegisterActivity();
+            }
+        });
     }
+
+    private void InitializeFields() {
+        LoginButton=(Button)findViewById(R.id.login_button);
+        PhoneLoginButton=(Button)findViewById(R.id.phone_login_button);
+        UserEmail=(EditText)findViewById(R.id.login_email);
+        UserPassword=(EditText)findViewById(R.id.login_password);
+        NeedNewAccountLink=(TextView)findViewById(R.id.need_new_account_link);
+        forgetPasswordLink=(TextView)findViewById(R.id.forget_password_link);
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -29,4 +55,9 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(loginIntent);
     }
 
+
+    private void SendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this,RegisterActivity.class);
+        startActivity(registerIntent);
+    }
 }
--- MononSoftLtd/app/src/main/java/com/mononsoft/mononsoftltd/RegisterActivity.java
+++ MononSoftLtd/app/src/main/java/com/mononsoft/mononsoftltd/RegisterActivity.java
@@ -3,9 +3,10 @@ package com.mononsoft.mononsoftltd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private Button CreateAccountButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
