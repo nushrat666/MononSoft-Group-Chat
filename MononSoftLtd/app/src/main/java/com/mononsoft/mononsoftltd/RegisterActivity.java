a1de225e3d0a74f4b99beefe7a91933f3a85753d
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2020-04-16 23:57:59 +0600

    mon

--- MononSoftLtd/app/src/main/java/com/mononsoft/mononsoftltd/RegisterActivity.java
+++ MononSoftLtd/app/src/main/java/com/mononsoft/mononsoftltd/RegisterActivity.java
@@ -10,10 +10,22 @@ import android.widget.TextView;
 public class RegisterActivity extends AppCompatActivity {
     private Button CreateAccountButton;
     private EditText UserEmail, UserPassword;
-    private TextView NeedNewAccountLink, forgetPasswordLink;
+    private TextView AlreadyHaveAccountLink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        InitializeFields();
+    }
+
+    private void InitializeFields() {
+
+        CreateAccountButton=(Button)findViewById(R.id.register_button);
+        UserEmail=(EditText)findViewById(R.id.register_email);
+        UserPassword=(EditText)findViewById(R.id.register_password);
+        AlreadyHaveAccountLink=(TextView)findViewById(R.id.need_new_account_link);
+
     }
 }
