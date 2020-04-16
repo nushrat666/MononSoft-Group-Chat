c987d78d45a5f44b1bb8dc0688c99b48178c8a8b
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2020-04-17 01:35:48 +0600

    mon

--- MononSoftLtd/app/src/main/java/com/mononsoft/mononsoftltd/MainActivity.java
+++ MononSoftLtd/app/src/main/java/com/mononsoft/mononsoftltd/MainActivity.java
@@ -8,10 +8,11 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     private Toolbar mToolbar;
     private ViewPager myViewPager;
     private TabLayout myTabLayout;
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser=mAuth.getCurrentUser();
 
         mToolbar=(Toolbar)findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
--- MononSoftLtd/app/src/main/java/com/mononsoft/mononsoftltd/RegisterActivity.java
+++ MononSoftLtd/app/src/main/java/com/mononsoft/mononsoftltd/RegisterActivity.java
@@ -1,31 +1,98 @@
 package com.mononsoft.mononsoftltd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
     private Button CreateAccountButton;
     private EditText UserEmail, UserPassword;
     private TextView AlreadyHaveAccountLink;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mAuth = FirebaseAuth.getInstance();
 
         InitializeFields();
+
+        AlreadyHaveAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToLoginActivity();
+            }
+        });
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreatNewAccount();
+            }
+        });
+    }
+
+    private void CreatNewAccount() {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter email...", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter password...", Toast.LENGTH_SHORT).show();
+        } else {
+            loadingBar.setTitle("Creating new Account!!");
+            loadingBar.setMessage("Please wait, untill we are creating new account for you....");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                SendUserToLoginActivity();
+                                Toast.makeText(RegisterActivity.this, "Account created Successfully!!!", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+                            }else{
+                                String message = task.getException().toString();
+                                Toast.makeText(RegisterActivity.this, "Error : " +message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
     }
 
     private void InitializeFields() {
 
-        CreateAccountButton=(Button)findViewById(R.id.register_button);
-        UserEmail=(EditText)findViewById(R.id.register_email);
-        UserPassword=(EditText)findViewById(R.id.register_password);
-        AlreadyHaveAccountLink=(TextView)findViewById(R.id.need_new_account_link);
+        CreateAccountButton = (Button) findViewById(R.id.register_button);
+        UserEmail = (EditText) findViewById(R.id.register_email);
+        UserPassword = (EditText) findViewById(R.id.register_password);
+        AlreadyHaveAccountLink = (TextView) findViewById(R.id.already_have_account_link);
+loadingBar=new ProgressDialog(this);
+    }
 
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
     }
 }
