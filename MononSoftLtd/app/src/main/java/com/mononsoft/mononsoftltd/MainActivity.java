9b6063a50ee0f7f5e50ff5ca7bdc829655f47974
Nushrat Jahan Tuli <nushrat.tuli666@gmail.com>
2020-04-16 21:25:20 +0600

    mon

--- MononSoftLtd/app/src/main/java/com/mononsoft/mononsoftltd/MainActivity.java
+++ MononSoftLtd/app/src/main/java/com/mononsoft/mononsoftltd/MainActivity.java
@@ -2,12 +2,18 @@ package com.mononsoft.mononsoftltd;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsAccessorAdapter myTabsAccessorAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +22,12 @@ public class MainActivity extends AppCompatActivity {
         mToolbar=(Toolbar)findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("MononSoft Ltd.");
+
+        myViewPager=(ViewPager)findViewById(R.id.main_tabs_pager);
+        myTabsAccessorAdapter=new TabsAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccessorAdapter);
+
+        myTabLayout=(TabLayout)findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
     }
 }
